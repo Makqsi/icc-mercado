2715b451743ebd4c3047b97ef173f970f3573558
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2016-10-10 20:47:08 -0300

    Update UI

--- src/com/github/palmeidaprog/iccmercado/main/Main.java
+++ src/com/github/palmeidaprog/iccmercado/main/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("TI - Mercado de Trabalho");
+        primaryStage.setScene(new Scene(root, 766, 432));
         primaryStage.show();
     }
 
