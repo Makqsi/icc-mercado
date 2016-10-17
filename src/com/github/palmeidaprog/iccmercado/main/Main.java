485c0126508201949299d203623b1237562b3270
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2016-10-17 04:57:16 -0300

    New UI - C#-like

--- src/com/github/palmeidaprog/iccmercado/main/Main.java
+++ src/com/github/palmeidaprog/iccmercado/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("TI - Mercado de Trabalho");
-        primaryStage.setScene(new Scene(root, 766, 432));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
