5453fcea8969f81d2244d36dd4d67c11d6c43240
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2016-11-09 15:07:58 -0300

    Creating UIEffects Class

---
+++ src/com/github/palmeidaprog/iccmercado/main/UIEffects.java
@@ -0,0 +1,11 @@
+/*
+@author Paulo Roberto Almeida Filho
+@email palmeidaprogramming@gmail.com
+Custom VBox properties
+*/
+
+package com.github.palmeidaprog.iccmercado.main;
+
+
+public class UIEffects {
+}
