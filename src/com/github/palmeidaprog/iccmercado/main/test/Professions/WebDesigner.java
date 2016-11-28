fd409baf12079d2889c7de4535dd8332a393b55d
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2016-11-28 02:20:35 -0300

    WebDesigner class and window

---
+++ src/com/github/palmeidaprog/iccmercado/main/test/Professions/WebDesigner.java
@@ -0,0 +1,177 @@
+package com.github.palmeidaprog.iccmercado.main.test.Professions;
+
+import com.github.palmeidaprog.iccmercado.main.Interfaces.Linkable;
+import com.github.palmeidaprog.iccmercado.main.Interfaces.Professionable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/*
+* Mercado de TI
+* Paulo R. Almeida Filho
+* pauloalmeidaf@gmail.com
+* */
+
+public class WebDesigner implements Professionable, Linkable {
+    // Links
+    private URL youtube;
+    private URL wikipedia;
+    private URL google;
+
+    // Requirements
+    private int hardware = 0;
+    private int lideranca = 0;
+    private int criatividade = 4;
+    private int ensino = 0;
+    private int relacionamento = 4;
+    private int pesquisa = 2;
+    private int matematica = 0;
+    private int logica = 0;
+    private int problemas = 3;
+    private int design = 4;
+    private int number = 5;
+    private int[] array = {hardware, lideranca, criatividade, ensino, relacionamento,
+            pesquisa, matematica, logica, problemas, design};
+    private int percentual = 0;
+
+    private final String AREA = "Design / Internet";
+    private final String PROFESSION = "Web Designer";
+
+
+    //--Singleton pattern--------------------------------------------------
+    private static volatile WebDesigner instance = null;
+
+    // constructor
+    private WebDesigner() {
+        try {
+            youtube = new URL("https://www.youtube.com/watch?v=XEwn68uHcDA");
+            wikipedia = new URL("https://pt.wikipedia.org/wiki/Web_design");
+            google = new URL("https://www.google.com.br/search?q=%22web+designer%22&oq=%22web+desi" +
+                    "gner%22&aqs=chrome..69i57j0l5.6158j0j4&sourceid=chrome&ie=UTF-8");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized static WebDesigner getInstance() {
+        if(instance == null) {
+            instance = new WebDesigner();
+        }
+        return instance;
+    }
+
+    //--Professionable methods----------------------------------------------------
+
+    @Override
+    public int[] getArray() {
+        return array;
+    }
+
+    @Override
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public int getHardware() {
+        return hardware;
+    }
+
+    @Override
+    public int getLideranca() {
+        return lideranca;
+    }
+
+    @Override
+    public int getCriatividade() {
+        return criatividade;
+    }
+
+    @Override
+    public int getEnsino() {
+        return ensino;
+    }
+
+    @Override
+    public int getRelacionamento() {
+        return relacionamento;
+    }
+
+    @Override
+    public int getPesquisa() {
+        return pesquisa;
+    }
+
+    @Override
+    public int getMatematica() {
+        return matematica;
+    }
+
+    @Override
+    public int getLogica() {
+        return logica;
+    }
+
+    @Override
+    public int getProblemas() {
+        return problemas;
+    }
+
+    @Override
+    public int getDesign() {
+        return design;
+    }
+
+    @Override
+    public Scene getDetails() {
+        FXMLLoader loaderResult = new FXMLLoader(getClass().getResource("suporte_tecnico.fxml"));
+        loaderResult.setController(WebDesignerController.getInstance());
+        Parent root = null;
+        try {
+            root = loaderResult.load();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene sceneReturn = new Scene(root, 750, 600);
+
+        return sceneReturn;
+    }
+
+    @Override
+    public String getArea() {
+        return AREA;
+    }
+
+    @Override
+    public String getProfession() {
+        return PROFESSION;
+    }
+
+    @Override
+    public int getPercentual() {
+        return percentual;
+    }
+
+    //--Linkable methods----------------------------------------------------
+
+    @Override
+    public URL getYoutube() {
+        return youtube;
+    }
+
+    @Override
+    public URL getWiki() {
+        return wikipedia;
+    }
+
+    @Override
+    public URL getSearch() {
+        return google;
+    }
+
+}
