7d24b808f9282fd35dcaafa274728ed5adc90c30
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2016-11-28 19:21:21 -0300

    Implemented AdminSistema class and window

---
+++ src/com/github/palmeidaprog/iccmercado/main/test/Professions/AdminSistema.java
@@ -0,0 +1,177 @@
+package com.github.palmeidaprog.iccmercado.main.test.Professions;
+
+import com.github.palmeidaprog.iccmercado.main.Interfaces.Linkable;
+import com.github.palmeidaprog.iccmercado.main.Interfaces.Professionable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/*
+* Mercado de TI
+* Paulo R. Almeida Filho
+* pauloalmeidaf@gmail.com
+* */
+
+public class AdminSistema implements Professionable, Linkable {
+    // Links
+    private URL youtube;
+    private URL wikipedia;
+    private URL google;
+
+    // Requirements
+    private int hardware = 4;
+    private int lideranca = 0;
+    private int criatividade = 0;
+    private int ensino = 0;
+    private int relacionamento = 2;
+    private int pesquisa = 3;
+    private int matematica = 0;
+    private int logica = 0;
+    private int problemas = 3;
+    private int design = 0;
+    private int number = 4;
+    private int[] array = {hardware, lideranca, criatividade, ensino, relacionamento,
+            pesquisa, matematica, logica, problemas, design};
+    private int percentual = 0;
+
+    private final String AREA = "Sistemas de Computação";
+    private final String PROFESSION = "Administrador de Sistemas";
+
+
+    //--Singleton pattern--------------------------------------------------
+    private static volatile AdminSistema instance = null;
+
+    // constructor
+    private AdminSistema() {
+        try {
+            youtube = new URL("");
+            wikipedia = new URL("https://pt.wikipedia.org/wiki/Administrador_de_sistemas");
+            google = new URL("https://www.google.com.br/webhp?sourceid=chrome-instant&ion=1&esp" +
+                    "v=2&ie=UTF-8#q=%22Administrador+de+Sistemas%22");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized static AdminSistema getInstance() {
+        if(instance == null) {
+            instance = new AdminSistema();
+        }
+        return instance;
+    }
+
+    //--Professionable methods----------------------------------------------------
+
+    @Override
+    public int[] getArray() {
+        return array;
+    }
+
+    @Override
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public int getHardware() {
+        return hardware;
+    }
+
+    @Override
+    public int getLideranca() {
+        return lideranca;
+    }
+
+    @Override
+    public int getCriatividade() {
+        return criatividade;
+    }
+
+    @Override
+    public int getEnsino() {
+        return ensino;
+    }
+
+    @Override
+    public int getRelacionamento() {
+        return relacionamento;
+    }
+
+    @Override
+    public int getPesquisa() {
+        return pesquisa;
+    }
+
+    @Override
+    public int getMatematica() {
+        return matematica;
+    }
+
+    @Override
+    public int getLogica() {
+        return logica;
+    }
+
+    @Override
+    public int getProblemas() {
+        return problemas;
+    }
+
+    @Override
+    public int getDesign() {
+        return design;
+    }
+
+    @Override
+    public Scene getDetails() {
+        FXMLLoader loaderResult = new FXMLLoader(getClass().getResource("admin_sistema.fxml"));
+        loaderResult.setController(AdminSistemaController.getInstance());
+        Parent root = null;
+        try {
+            root = loaderResult.load();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        Scene sceneReturn = new Scene(root, 750, 600);
+
+        return sceneReturn;
+    }
+
+    @Override
+    public String getArea() {
+        return AREA;
+    }
+
+    @Override
+    public String getProfession() {
+        return PROFESSION;
+    }
+
+    @Override
+    public int getPercentual() {
+        return percentual;
+    }
+
+    //--Linkable methods----------------------------------------------------
+
+    @Override
+    public URL getYoutube() {
+        return youtube;
+    }
+
+    @Override
+    public URL getWiki() {
+        return wikipedia;
+    }
+
+    @Override
+    public URL getSearch() {
+        return google;
+    }
+
+}
