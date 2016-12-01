e97ff4060e066968215206d65385e68fa7e1a7b3
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2016-12-01 16:58:22 -0300

    Sync save: implementing vbox_lei FXMLs

--- src/com/github/palmeidaprog/iccmercado/main/ControllerLei.java
+++ src/com/github/palmeidaprog/iccmercado/main/ControllerLei.java
@@ -1,15 +1,69 @@
 package com.github.palmeidaprog.iccmercado.main;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
 /**
  * Created by paulo on 10/27/16.
  */
 public class ControllerLei {
+    @FXML private VBox leiVB1, leiVB2;
+    @FXML private Button btnVoltar, btnAvancar;
+    @FXML private BorderPane mainLeiVB;
+    private int box = 0;
+    private UIEffects e = UIEffects.getInstance();
 
-    public void retornarBtnClickAction() {
-        //todo: implement
-    }
-
+    // Evento de clique do botão avançar
+    // Avançar button click event
     public void avancarBtnClickAction() {
-        //todo: implement
+        switch(box) {
+            case 0:
+                btnVoltar.setDisable(false);
+                avancar(leiVB2);
+                break;
+            default:
+                break;
+        }
     }
+
+    // Evento de clique do botão avançar
+    // Avançar button click event
+    public void retornarBtnClickAction() {
+        switch(box) {
+            case 0:
+                break;
+            case 1:
+                retornar(leiVB1);
+                btnVoltar.setDisable(true);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // change VBox and using appropriate effects
+    // mudar VBox e utilizar o efeitos apropriados
+    private void avancar(VBox vB) {
+        box++;
+        e.fadeBoxes(mainLeiVB.getCenter(), 1);
+        mainLeiVB.setCenter(vB);
+        e.fadeBoxes(mainLeiVB.getCenter(), 0);
+        e.trans(vB);
+    }
+
+    // change VBox and using appropriate effects
+    // mudar VBox e utilizar o efeitos apropriados
+    private void retornar(Node vB) {
+        box--;
+        e.fadeBoxes(mainLeiVB.getCenter(), 1);
+        mainLeiVB.setCenter(vB);
+        e.fadeBoxes(mainLeiVB.getCenter(), 0);
+        e.transBack(vB);
+    }
+
 }
